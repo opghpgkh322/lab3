da15249fabc34964836b666f3b5e2265af551aa5
unknown <opghpgkh322@yandex.ru>
2020-12-11 09:41:48 +0300

    '123'

--- lab3.py
+++ lab3.py
@@ -1,25 +1,22 @@
 from struct import pack
 from math import sin, cos, pi
 
-width = 600
-height = 600
-t = 0
-STEP = 0.005
-pixels = []
-x_min = float("inf")
-y_min = float("inf")
 
-while t <= 10 * pi:
-    x = round(2 * (sin(2 * t) - cos(t)/2), 2)
-    if x < x_min:
-        x_min = x
-    y = round(2 * (cos(2 * t) - sin(t)/2), 2)
-    if y < y_min:
-        y_min = y
-    pixels.append((x, y))
-    t += STEP
-
-pixels.reverse()
+def create_pixels(start, stop, step):
+    pixels = []
+    x_min = y_min = float("inf")
+    t = start
+    while t <= stop:
+        x = round(2 * (sin(2 * t) - cos(t) / 2), 2)
+        if x < x_min:
+            x_min = x
+        y = round(2 * (cos(2 * t) - sin(t) / 2), 2)
+        if y < y_min:
+            y_min = y
+        pixels.append((x, y))
+        t += step
+    pixels.reverse()
+    return pixels, x_min, y_min
 
 
 def create_bitmap_header(wigth, height):
@@ -30,14 +27,32 @@ def create_bitmap_header(wigth, height):
     filesize = offset + 1 * wigth * height
     return pack("<HL2HL", filetype, filesize, reserved_1, reserved_2, offset)
 
-def create_info_header(wigth, height):
+
+def create_info_header(width, height):
     header_size = 40
     planes = 1
     bits_per_pixel = 8
     compression = 0
     image_size = 0
+    x_pixels_per_meter = 0
+    y_pixels_per_meter = 0
     total_colors = 2
-    return pack("<3L2H3L", header_size, wigth, height, planes, bits_per_pixel, compression, image_size, total_colors)
+    important_colors = 0
+    return pack(
+        "<3L2H6L",
+        header_size,
+        width,
+        height,
+        planes,
+        bits_per_pixel,
+        compression,
+        image_size,
+        x_pixels_per_meter,
+        y_pixels_per_meter,
+        total_colors,
+        important_colors,
+    )
+
 
 def create_color_pallete():
     color_1 = (0, 0, 0, 0)
@@ -45,18 +60,24 @@ def create_color_pallete():
     return pack("<8B", *color_1, *color_2)
 
 
-with open ("img.bmp", "wb") as f:
-    f.write(create_bitmap_header(width, height))
-    f.write(create_info_header(width, height))
-    f.write(create_color_pallete())
+def write_file(start, stop, step, width, height, filename):
+    with open("{}.bmp".format(filename), "wb") as f:
+        f.write(create_bitmap_header(width, height))
+        f.write(create_info_header(width, height))
+        f.write(create_color_pallete())
+        pixels, x_min, y_min = create_pixels(start, stop, step)
 
-    y_pix = y_min
-    for y_val in range(height):
-        x_pix = x_min
-        for x_val in range(width):
-            if (x_pix, y_pix) in pixels:
-                f.write(pack("<B", 0))
-            else:
-                f.write(pack("<B", 1))
-            x_pix = round(x_pix + STEP, 2)
-        y_pix = round(y_pix + STEP, 2)
+        y_pix = y_min
+        for i in range(height):
+            x_pix = x_min
+            for j in range(width):
+                if (x_pix, y_pix) in pixels:
+                    f.write(pack("<B", 0))
+                else:
+                    f.write(pack("<B", 1))
+                x_pix = round(x_pix + step, 2)
+            y_pix = round(y_pix + step, 2)
+
+
+if __name__ == "__main__":
+    write_file(0, 10 * pi, 0.01, 600, 600, "img")
